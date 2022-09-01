bcdbe6d62d479255f5a17d967570867e9fdfc45a
Artur-0 <romeroarturo597@gmail.com>
2022-09-01 15:26:09 -0300

    refactor(src/redux): changes in Rtoolkit

--- client/src/Components/Store/index.jsx
+++ client/src/Components/Store/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../../redux/actions/productsAction";
+import { getProducts } from "../../redux/actions/productsActions";
 
 function Store() {
   const dispatch = useDispatch();
