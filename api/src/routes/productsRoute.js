6f8da1f83c5941398362b4d3050dd212c12d3987
Barbara Borini <borini.barbara@gmail.com>
2022-09-02 18:38:36 -0300

    'asd'

--- api/src/routes/productsRoute.js
+++ api/src/routes/productsRoute.js
@@ -12,17 +12,38 @@ router.get('/', async (req, res) => {
     }
 });
 
+
 router.post('/add', async (req, res) => {
+  const { name, price, idcategory, image, description, active} = req.body;
   try {
-    const newProduct = new Product(req.body);
-    await newProduct.save();
+    const newProduct = await Product.create({
+      name, 
+      price, 
+      image, 
+      description, 
+      active,
+      idcategory
+    });
+    // const selectCategory = await Category.findOne({
+    // where: {id: idcategory}
+    // })
+    // await newProduct.addCategories(selectCategory);
+    // then (()=>{
+    //   console.log(selectCategory)
+    // });
+    // await newProduct.addCategory(category);
+    // const newProduct = new Product(req.body);
+    // await newProduct.save();
     res.status(200).json({ message: "Product succefully created" });
   } catch (error) {
     res.status(404).json({ message: "Cant create product" });
   }
 });
 
-
+// let gByGame = await Genero.findAll({
+//       where: { name: genre },
+//     });
+//     CreatteGame.addGenero(gByGame);
 
 module.exports = router;
 
