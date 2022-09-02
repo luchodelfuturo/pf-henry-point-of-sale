dfe090b82d95a9f3f103a2bc08c5289c014a8870
EnriqueSixto01 <enriquesixto95@gmail.com>
2022-09-02 16:17:10 -0500

    update

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -3,12 +3,6 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
     sequelize.define('product', {
-        Id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false,
-            primaryKey: true
-        },
         name: {
             type: DataTypes.STRING(50),
             allowNull: false,
