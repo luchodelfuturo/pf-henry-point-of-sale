0d37791dc881fa7dad5e7c828235ca29b569b50e
Marco pistagnesi <espadachin2003@gmail.com>
2022-09-02 16:55:44 -0300

    productos precargados

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,9 +8,6 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-// prueba
-// otro cambio
-// cambio3
 
 router.get("/orders", async (req, res) => {
   let results = [];
