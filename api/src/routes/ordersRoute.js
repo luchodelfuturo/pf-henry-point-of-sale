80946205f962ff665a1f28112abd2ade30e1a0e0
EnriqueSixto01 <enriquesixto95@gmail.com>
2022-09-04 17:54:12 -0500

    component counter

--- api/src/routes/ordersRoute.js
+++ api/src/routes/ordersRoute.js
@@ -21,17 +21,6 @@ router.put("/put/:orderNumber", async (req, res) => {
   return res.send("status updated");
 });
 
-// router.get("/ready", async (req, res) => {
-//   let results = [];
-//   results = await Order.findAll({ where: { status: "r" }, include: Product });
-
-//   if (results.length === 0) {
-//     res.status(404).json("No se encontraron resultados");
-//   } else {
-//     res.status(200).json(results);
-//   }
-// });
-
 router.post("/", async (req, res) => {
   try {
     const order = await Order.create(req.body);
