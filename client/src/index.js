fafbec23c78e61e5fe768030aa53b3df98fd5faf
Artur-0 <romeroarturo597@gmail.com>
2022-09-13 11:13:18 -0300

    para vercel

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
-
+//para vercel
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
