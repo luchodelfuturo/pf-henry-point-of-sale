b3dd7f022a2750ee8171d3136baa1c85930a37ff
luchocas234 <lfcastro2911@gmail.com>
2022-09-21 19:10:15 -0300

    s

--- client/src/redux/slices/cashflowSlice.js
+++ client/src/redux/slices/cashflowSlice.js
@@ -1,55 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 export const cashFlowSlice = createSlice({
     name: "cashFlow",
     initialState: {
-        lastCashFlow: []
-        // totalCash: [],
-        // totalPaypal: [],
-        // totalIncome: [],
-        // totalExpenses: [],
-        // totalSales: [],
-        // cashInit: [],
-        // totalAll: [],
+        lastCashFlow: [],
+        allCashFlow: [],
+        filteredCashFlow: [],
+
     },
     reducers: {
-        // ordersFinishedCash: (state, action) => {
-        //   state.totalCash = action.payload;
-        // },
+        
         getLastCashFlow: (state, action) => {
             state.lastCashFlow = action.payload;
+        },
+        getAllCashFlow: (state, action) => {
+            state.allCashFlow = action.payload;
+            state.filteredCashFlow = action.payload;
+        },
+        searchDateCashFlow: (state, action) => {
+            const filtro = state.filteredCashFlow;
+
+            state.allCashFlow = filtro.filter(cash => cash.date === action.payload)
         }
-        // ordersFinishedPaypal: (state, action) => {
-        //   state.totalPaypal = action.payload;
-        // },
-        // getTotalIncome: (state, action) => {
-        //   state.totalIncome = action.payload;
-        // },
-        // getTotalExpense: (state, action) => {
-        //   state.totalExpenses = action.payload;
-        // },
-        // getTotalSales: (state, action) => {
-        //   state.totalSales = action.payload;
-        // },
-        // addCashInit: (state, action) => {
-        //   console.log(action.payload);
-        //   state.cashInit = [action.payload];
-        // },
-        // getTotal: (state, action) => {
-        //   state.totalAll = action.payload;
-        // },
-    },
+        },
 });
 
 export const {
-    //   getTotalIncome,
-    //   getTotalExpense,
-    //   getTotalSales,
-    //   addCashInit,
-    //   ordersFinishedCash,
-    //   ordersFinishedPaypal,
-    //   getTotal,
-    getLastCashFlow
-} = cashFlowSlice.actions;
+    getLastCashFlow,
+    getAllCashFlow,
+    searchDateCashFlow } = cashFlowSlice.actions;
 
-export default cashFlowSlice.reducer;
+export default cashFlowSlice.reducer; 
