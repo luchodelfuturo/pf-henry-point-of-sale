467ad7a95ecfd339e4b0f61d7b75aeb7a92598ee
luchocas234 <lfcastro2911@gmail.com>
2022-09-22 11:35:35 -0300

    cambiando REACT APP API por url back

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 //para vercel
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = "https://henrybar-pointofsale.herokuapp.com" || "http://localhost:3001";
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
