8354a94329af049789af8a739d9a18a2da8b9b9c
luchocas234 <lfcastro2911@gmail.com>
2022-09-22 12:16:22 -0300

    s

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -75,13 +75,13 @@ const userCtrl = {
 
             const isMatch = await bcrypt.compare(password, user.password)
             if (!isMatch) return res.status(400).json({ msg: "Password is incorrect." })
-
-            const refresh_token = createRefreshToken({ id: user.id })
-            res.cookie('refreshtoken', refresh_token, {
-                httpOnly: false,
-                path: '/users/refresh_token',
-                maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-            })
+        //   (newUser) createRefreshToken
+            const refresh_token = createActivationToken({ id: user.id })
+            // res.cookie('refreshtoken', refresh_token, {
+            //     httpOnly: false,
+            //     path: '/users/refresh_token',
+            //     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+            // })
 
             res.json({ msg: "Login success!" })
         } catch (err) {
