a8ea1f65208372d574dca176f0c652eeb0bd00a7
luchocas234 <lfcastro2911@gmail.com>
2022-09-21 23:42:29 -0300

    dotenv front

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,11 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+//para vercel
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
