18a4f9a6d7afa2acd8e739276f065c25a17add9e
juliang <36992361+juliangalvez@users.noreply.github.com>
2022-09-22 13:31:01 -0300

    Delete api directory

--- api/src/routes/uploadRoute.js
+++
@@ -1,8 +0,0 @@
-// const router = require('express').Router()
-// const uploadImage = require('../middleware/uploadImage')
-// const uploadCtrl = require('../controllers/upload')
-// const auth = require('../middleware/auth')
-
-// router.post('/', uploadImage, auth, uploadCtrl.uploadAvatar)
-
-// module.exports = router
